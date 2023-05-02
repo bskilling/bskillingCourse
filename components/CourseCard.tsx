8909abded35558a903db95af908af8f224fdcf91
JEREMIA-SUNNY <you@example.com>
2023-05-02 14:44:07 +0530

    iamge bug is fixed

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -17,6 +17,7 @@ interface CourseCardProps {
   };
 }
 const CourseCard: React.FC<CourseCardProps> = ({ data }) => {
+  console.log(data.imageSrc);
   return (
     <>
       <section className="">
@@ -83,9 +84,11 @@ const CourseCard: React.FC<CourseCardProps> = ({ data }) => {
                 </p>
               </div>
               <div>
-                <button className="px-2 py-2 text-white bg-buttonBlue rounded-lg bg-buttonBlue hover:bg-buttonBlue focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                  View Course
-                </button>
+                <a target="_blank" rel="noreferrer" href={data.CourseLink}>
+                  <button className="px-2 py-2 text-white bg-buttonBlue rounded-lg  hover:bg-buttonBlue focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    View Course
+                  </button>
+                </a>
               </div>
             </div>
           </div>
