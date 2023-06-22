616c375ba095749f08bd5f32beabf54da333b484
JEREMIA-SUNNY <you@example.com>
2023-06-22 13:18:27 +0530

    new nav on landing page is added

--- src/pages/courses/sap/sapbtp.tsx
+++ src/pages/courses/sap/sapbtp.tsx
@@ -1,26 +1,17 @@
-import {
-  BsPeople,
-  BsBookmarkCheck,
-  BsFillPeopleFill,
-  BsTelephone,
-} from "react-icons/bs";
-import Link from "next/link";
-import { BiTime } from "react-icons/bi";
-import { FaCalendarTimes, FaMedal } from "react-icons/fa";
-import { AnimatePresence, motion, spring } from "framer-motion";
-import { GiPriceTag } from "react-icons/gi";
 import Accordion from "components/accordion";
-import Head from "next/head";
-import { useState } from "react";
-import { useEffect } from "react";
-import { useForm } from "react-hook-form";
-import "react-phone-number-input/style.css";
-import PhoneInput from "react-phone-number-input";
-import DropAQueryForm from "modules/leadChat/components/DropAQueryForm";
-import CourseCard from "components/CourseCard";
+import AccordionFaq from "components/accordionFaq";
 import CourseSlider from "components/courseSlider";
 import InsideCourse from "components/insideCourseSubpoint";
-import AccordionFaq from "components/accordionFaq";
+import LandingPageFooter from "components/landingPageFooter";
+import { motion } from "framer-motion";
+import DropAQueryForm from "modules/leadChat/components/DropAQueryForm";
+import Head from "next/head";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { BiTime } from "react-icons/bi";
+import { BsFillPeopleFill, BsTelephone } from "react-icons/bs";
+import "react-phone-number-input/style.css";
 interface accord {
   question: string;
   answer: string;
@@ -42,6 +33,28 @@ const SapBtps = () => {
   const transition2 = { type: "spring", duration: 1 };
   const [messageSent, setMessage] = useState(false);
   const [CountryCodeValue, setCountryCodeValue] = useState<any>("+91");
+
+  const [showFixedLandingFooter, setShowFixedLandingFooter] = useState(false);
+  useEffect(() => {
+    function handleScroll() {
+      const scrollPosition = window.scrollY;
+      const halfScreenHeight = window.innerHeight / 4.9;
+
+      if (scrollPosition >= halfScreenHeight) {
+        setShowFixedLandingFooter(true);
+      } else {
+        setShowFixedLandingFooter(false);
+      }
+    }
+
+    // Add the event listener when the component mounts
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   const {
     register,
     handleSubmit,
@@ -130,7 +143,7 @@ const SapBtps = () => {
           }}
         />
       </Head>
-
+      {showFixedLandingFooter && <LandingPageFooter />}
       <section className="bg-gray font-SourceSans">
         {/* mobile view  above*/}
         <section>
@@ -156,13 +169,13 @@ const SapBtps = () => {
                       SAP Business Technology Platform
                     </p>
                   </div>
-                  <div className="flex justify-center">
+                  {/* <div className="flex justify-center">
                     <img
                       src="/crsInside/saplogo.png"
                       className="w-[200px] h-[200px]"
                       alt=""
                     />
-                  </div>
+                  </div> */}
                 </div>
                 <motion.div
                   // initial={{ y: "200px" }}
@@ -294,11 +307,12 @@ const SapBtps = () => {
             className=" flex  flex-col -violet-600 w-full 0   md:flex-row justify-center  gap-10"
           >
             {/* main text content here */}
-            <div className="  flex flex-col md:flex-row gap-5  md:basis-[90%] ">
+            <div className="  flex flex-col  md:flex-row gap-5  md:basis-[90%] ">
               <div className="flex flex-col md:flex-1  gap-5 h-fit    ">
-                <div className="bg-white px-5 md:px-6 w-full min-h-[1220px]  pb-8  rounded-xl">
+                <div className="bg-white px-5 md:px-12 w-full min-h-[1220px]  pb-8  rounded-xl">
+                  <div id="Overview" className="h-12 "></div>
                   <div>
-                    <p className="mt-9 text-2xl font-bold   mb-4">Overview</p>
+                    <p className="mt-2 text-2xl font-bold   mb-4">Overview</p>
                     <p className="   ">
                       SAP Business Technology Platform (SAP BTP) is a
                       cloud-based platform that enables businesses to build,
@@ -319,9 +333,10 @@ const SapBtps = () => {
                       instant access to business-context-rich information
                       from your SAP and third-party systems.
                     </p>
-                    <div className="mt-2 flex md:flex-row flex-col">
+                    <div id="Objectives" className="h-12 "></div>
+                    <div className=" flex md:flex-row flex-col">
                       <div className="flex-1">
-                        <p className="pt-8 pb-4 text-xl md:text-left font-semibold">
+                        <p className="_pt-8 pb-4 text-xl md:text-left font-semibold">
                           Objectives
                         </p>
                         <div className="flex gap-1 pt-2    ">
@@ -358,12 +373,24 @@ const SapBtps = () => {
                           business operations
                         </div>
                       </div>
-                      <div className="md:w-[400px] justify-center  pt-8 items-center  flex flex-row  md:gap-10">
-                        <div>
+                      <div className="md:w-[500px] justify-end  mr-2 _pt-8 items-end  flex flex-row gap-3">
+                        {/* <div>
                           <a className="play-btn" onClick={handleClick}></a>
+                        </div> */}
+                        <div className="md:w-fit flex-3 w-full  h-[300px]  md:h-fit flex justify-center">
+                          <iframe
+                            className="rounded-lg"
+                            width="400"
+                            height="230"
+                            src="https://www.youtube.com/embed/kWUcfALH1rk"
+                            title="YouTube video player"
+                            frameBorder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                            allowFullScreen
+                          ></iframe>
                         </div>
 
-                        {showVideo && (
+                        {/* {showVideo && (
                           <div
                             className={`fixed flex justify-center items-center bg-opacity-80  z-[2000] top-0   left-0 w-screen h-screen bg-black`}
                           >
@@ -391,15 +418,13 @@ const SapBtps = () => {
                               </div>
                             </div>
                           </div>
-                        )}
-                        <div className="font-semibold md:mt- text-blue-600">
-                          Watch <br /> Intro Video
-                        </div>
+                        )} */}
                       </div>
                     </div>
+                    <div id="Eligibility" className="h-12 "></div>
                     <div className="flex w-full md:flex-row gap-20 flex-col  justify-center">
                       <div className="flex flex-col   md:w-[50%]">
-                        <p className="pt-8 pb-4 text-xl md:text-left font-semibold ">
+                        <p className="pt- pb-4 text-xl md:text-left font-semibold ">
                           Eligibility
                         </p>
                         <p className="text-justify   ">
@@ -430,8 +455,8 @@ const SapBtps = () => {
                           </span>{" "}
                           Experience in deploying applications to SAP BTP.
                         </div>
-
-                        <p className="pt-8  text-xl  pb-4 font-semibold">
+                        <div id="KeyFeatures" className="h-12 "></div>
+                        <p className="_pt-8  text-xl  pb-4 font-semibold">
                           Key Features
                         </p>
 
@@ -491,43 +516,43 @@ const SapBtps = () => {
 
                       <div className="flex flex-col md:w-[50%]">
                         <div className=" rounded-md px-4 pb-4 ">
-                          <p className="pt-8 pb-4 text-xl font-semibold">
+                          <p className="_pt-8 pb-4 text-xl text-right font-semibold">
                             Skills Covered
                           </p>
-                          <div className="flex md:grid flex-wrap md:grid-cols-2 flex-col gap-5">
-                            <div className="border-2 rounded-md border-blue-600 text-center px-2 py-2 text-buttonBlue">
+                          <div className="flex md:flex flex-wrap  justify-end items-end   flex-col gap-5">
+                            <div className="border rounded-md w-fit border-buttonBlue text-center px-2 py-2 text-buttonBlue">
                               API Management
                             </div>
-                            <div className="border-2 rounded-md border-blue-600 text-center px-2 py-2 text-buttonBlue">
+                            <div className="border rounded-md border-buttonBlue text-center px-2 py-2 text-buttonBlue">
                               Cloud Integration
                             </div>
 
-                            <div className="border-2 rounded-md border-blue-600 text-center px-2 py-2 text-buttonBlue">
+                            <div className="border rounded-md border-buttonBlue text-center px-2 py-2 text-buttonBlue">
                               Cloud Transport Management
                             </div>
 
-                            <div className="border-2 rounded-md border-blue-600 text-center px-2 py-2 text-buttonBlue">
+                            <div className="border rounded-md border-buttonBlue text-center px-2 py-2 text-buttonBlue">
                               Integration Advisor
                             </div>
 
-                            <div className="border-2 rounded-md border-blue-600 text-center px-2 py-2 text-buttonBlue">
+                            <div className="border rounded-md border-buttonBlue text-center px-2 py-2 text-buttonBlue">
                               Integration Assessment
                             </div>
 
-                            <div className="border-2 rounded-md border-blue-600 text-center px-2 py-2 text-buttonBlue">
+                            <div className="border rounded-md border-buttonBlue text-center px-2 py-2 text-buttonBlue">
                               SAP Integration Suite
                             </div>
-                            <div className="border-2 rounded-md border-blue-600 text-center px-2 py-2 text-buttonBlue">
+                            <div className="border rounded-md border-buttonBlue text-center px-2 py-2 text-buttonBlue">
                               Trading Partner Management
                             </div>
                           </div>
                         </div>
                       </div>
                     </div>
-
+                    <div id="Benefits" className="h-12 "></div>
                     <div className="flex w-full md:flex-row flex-col  gap-4 justify-center">
                       <div className="flex flex-col  md:w-[50%]">
-                        <p className="pt-8 pb-4 text-xl  font-semibold">
+                        <p className="_pt-8 pb-4 text-xl  font-semibold">
                           Benefits
                         </p>
 
@@ -569,14 +594,17 @@ const SapBtps = () => {
                       <div className="flex flex-col  md:w-[50%]"></div>
                     </div>
                     <div>
+                      <div id="Resources" className="h-12 "></div>
                       <InsideCourse />
                     </div>
                   </div>
 
                   {/* tabsssssssssssssssssssssssssssssssssssssssss */}
                 </div>
-                <div className="bg-white px-5   w-full   pb-8  rounded-xl">
-                  <p className="mt-9 text-2xl font-bold   mb-4">Curriculum</p>
+
+                <div className="bg-white px-12   w-full   pb-8  rounded-xl">
+                  <div id="Curriculum" className="h-12 "></div>
+                  <p className="_mt-9 text-2xl font-bold   mb-4">Curriculum</p>
                   <div className="bg-white flex-1  w-full   pb-8  rounded-xl">
                     {" "}
                     <div className="w-full ">
@@ -648,9 +676,10 @@ const SapBtps = () => {
                     </div>
                   </div>
                 </div>
-                <div className="bg-white px-5 md:px-6 w-full   pb-8  rounded-xl">
+                <div className="bg-white px-5 md:px-12 w-full   pb-8  rounded-xl">
                   {" "}
-                  <p className="mt-9 text-2xl font-bold   mb-4">OutComes</p>
+                  <div id="OutComes" className="h-12 "></div>
+                  <p className="_mt-9 text-2xl font-bold   mb-4">OutComes</p>
                   <div className=" flex  w-full flex-col">
                     <div className="flex   gap-1   ">
                       <span className="text-blue-600 font-extrabold ">-</span>
@@ -685,9 +714,10 @@ const SapBtps = () => {
                     </div>
                   </div>
                 </div>
-                <div className=" px-5 md:px-6 w-full bg-white  pb-8  rounded-xl">
+                <div className=" px-5 md:px-12 w-full bg-white  pb-8  rounded-xl">
                   {" "}
-                  <p className="mt-9 text-2xl font-bold   mb-4">
+                  <div id="Certification" className="h-12 "></div>
+                  <p className="_mt-9 text-2xl font-bold   mb-4">
                     Certification
                   </p>
                   <div className="md:bg-white   pb-8 w-full  rounded-xl">
@@ -730,9 +760,10 @@ const SapBtps = () => {
                   </div>
                 </div>
 
-                <div className="bg-white px-5 md:px-6 w-full   pb-8  rounded-xl">
+                <div className="bg-white px-5 md:px-12 w-full   pb-8  rounded-xl">
                   {" "}
-                  <p className="mt-9 text-2xl font-bold   mb-4">FAQs</p>
+                  <div id="FAQs" className="h-12 "></div>
+                  <p className="_mt-9 text-2xl font-bold   mb-4">FAQs</p>
                   <div className="bg-white  w-full pt-2  pb-8  rounded-xl">
                     <div>
                       <AccordionFaq
