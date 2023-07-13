32ba17387d258afdd8a6c32b1effa076c1bbe1f6
JEREMIA-SUNNY <you@example.com>
2023-07-13 14:44:58 +0530

    new commit

--- src/pages/dummy.tsx
+++ src/pages/dummy.tsx
@@ -1,7 +1,22 @@
+import axios from "axios";
+import { GetServerSidePropsContext } from "next";
 import React from "react";
-
-function Dummy() {
+interface IDummyProps {
+  query: unknown;
+  param: unknown;
+}
+function Dummy(props: IDummyProps) {
+  console.log(props);
   return <div>dummy</div>;
+}
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  console.log(JSON.stringify(context.query));
+  return {
+    props: {
+      query: context.query || {},
+      param: context.params || {},
+    },
+  };
 }
 
 export default Dummy;
