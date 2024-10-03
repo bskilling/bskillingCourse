5787bc875abc5616f4ba6b82c63e6060922a289c
hmt4 <45815721+hmt4@users.noreply.github.com>
2024-10-03 13:45:05 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -206,7 +206,7 @@ const Footer = () => {
 
         <div className="bg-gray w-full mt-6">
           <p className="text-footerText text-center mx-auto max-w-screen-xl py-6">
-            Copyright bSkilling © 2024. All Rights Reserved. Designed by
+            Copyright ©️ BSKILLING PRIVATE LIMITED.All Rights Reserved. Designed by
             Stimulus Research Services
           </p>
         </div>
