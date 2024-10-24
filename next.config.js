7f2a8d006d513a7370d134179f9758de68e9314f
harmeetsh <harmeet@hutechsolutions.com>
2024-10-24 23:25:54 +0530

    space issue

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["https://www.bskilling.com/"],
+    loader: "akamai",
+    path: "/",
+  },
   env: {
     PUBLIC_URL: "/",
   },
