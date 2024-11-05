9b1b7fda0c323593b33733291b94daae7508634e
hmt4 <45815721+hmt4@users.noreply.github.com>
2024-11-05 09:48:14 +0530

    google tag code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -121,7 +121,23 @@ function MyApp({ Component, pageProps }: AppProps) {
           <meta name="twitter:card" content="summary" />
           {/* Twitter meta tags end */}
 
+          <meta name="google-site-verification" content="l3AerOmZFQ8qlPqtZ64bYdtXuKO2QrMD_R1jtzpSlVo" />
+
           <link rel="icon" href="/favicon.png" />
+
+            <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5H7RHNRM');`,
+            }}
+          />
+
+
+
+          
           {/* google tag manager start */}
           <script
             async
@@ -188,7 +204,13 @@ function MyApp({ Component, pageProps }: AppProps) {
           ></script>
           
         </Head>
+<body>
 
+ 
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5H7RHNRM"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
         <Component {...pageProps} />
         {/* <FloatWindow /> */}
 
@@ -203,6 +225,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             <FixedFooterBar />
           </motion.div>
         )}
+  </body>
       </Layout>
     </MyProvider>
   );
