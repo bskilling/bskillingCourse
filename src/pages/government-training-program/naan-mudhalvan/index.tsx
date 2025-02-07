609fcdc4e15cd5a4e96930bca17b5ea82750a102
sfjgit <ta7179@sfjbs.com>
2025-02-07 12:34:01 +0530

    added emails

--- src/pages/government-training-program/naan-mudhalvan/index.tsx
+++ src/pages/government-training-program/naan-mudhalvan/index.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Head from 'next/head';
 import NavbarSection from 'components/navbar/NavbarSection';
-import { FaCircle } from 'react-icons/fa';
+import { FaCircle, FaHandPointRight } from 'react-icons/fa';
 import {
   Card,
   CardContent,
@@ -650,6 +650,21 @@ export default function NaanMudhalvan() {
             <p className="mt-4">
               Take the first step toward a successful career today!
             </p>
+          </div>
+          <div className="mt-10">
+            <div className="mt-5 flex gap-x-3 items-center justify-center">
+              <FaHandPointRight
+                size={50}
+                className="animate-pulse text-destructive"
+              />
+              Email:
+              <a
+                href="mailto:contact.naanmudhalvan@bskilling.com"
+                className="text-primary "
+              >
+                contact.naanmudhalvan@bskilling.com
+              </a>
+            </div>
           </div>
         </div>
       </div>
