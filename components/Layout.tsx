77c1a29157a2cb76f79553d7c08bacadeb28f1ff
sfjgit <ta7179@sfjbs.com>
2025-02-13 17:53:57 +0530

    rahul

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -278,7 +278,10 @@ const Layout = ({ children, pageTitle = 'bSkilling' }: Props) => {
                           <SheetTitle></SheetTitle>
                           <SheetDescription></SheetDescription>{' '}
                           {dropSearchData.map((course, index) => (
-                            <Link href={course?.url} key={index}>
+                            <Link
+                              href={'courses/course-details/' + course?.url}
+                              key={index}
+                            >
                               <div className="p-2 text-black hover:bg-buttonBlue px-5 hover:text-primary cursor-pointer">
                                 {course.title}
                               </div>
