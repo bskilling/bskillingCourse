28e6d3aac1997743c6b05f8ae0cf6a800b13c290
sfjgit <ta7179@sfjbs.com>
2025-03-11 18:48:49 +0530

    neew footer and glosy design

--- src/components/pages/GoogleReviews.tsx
+++ src/components/pages/GoogleReviews.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 'use client';
 import { FaStar } from 'react-icons/fa';
 import { FaRegStarHalfStroke } from 'react-icons/fa6';
@@ -9,11 +10,11 @@ export default function GoogleReviews() {
   const totalReviews = 34;
 
   return (
-    <div className="max-w-md bg-transparent text-white  rounded-xl p-4 pr-8 pt-0">
+    <div className="max-w-md bg-transparent text-white  rounded-xl pt-0 mt-5">
       {/* Header */}
       <div className="flex items-center gap-2 mb-2">
         <FcGoogle size={22} />
-        <h2 className="text-lg font-semibold">Google Reviews</h2>
+        <h2 className="text-sm">Google Reviews</h2>
       </div>
 
       {/* Rating Overview */}
@@ -31,23 +32,49 @@ export default function GoogleReviews() {
       {/* Review Link */}
       <div className="mt-3">
         <a
-          href={`https://search.google.com/local/reviews?placeid=${placeId}`}
+          href={`https://g.page/r/CXYsGIz6BzjzEBM/review`}
           target="_blank"
           rel="noopener noreferrer"
           className="text-blue-400  text-sm"
         >
-          ({totalReviews}) View More Reviews on Google
+          Rate Us on Google
         </a>
+      </div>
+      <div className="flex gap-4 flex-wrap mt-5">
+        {[
+          {
+            name: 'Instagram',
+            icon: '/icon/insta.svg',
+            link: 'https://www.instagram.com/bskillingindia/',
+          },
+          {
+            name: 'Facebook',
+            icon: '/icon/facebook.svg',
+            link: 'https://www.facebook.com/bskillingindia/',
+          },
+          {
+            name: 'LinkedIn',
+            icon: '/icon/link.svg',
+            link: 'https://www.linkedin.com/company/bskillingindia/',
+          },
+          {
+            name: 'Twitter',
+            icon: '/twitter.webp',
+            link: 'https://twitter.com/b_SkillingIndia',
+          },
+        ].map((social, idx) => (
+          <a
+            key={idx}
+            href={social.link}
+            target="_blank"
+            className="hover:opacity-75 transition"
+          >
+            <img src={social.icon} alt={social.name} className="w-7 h-7" />
+          </a>
+        ))}
       </div>
 
       {/* Address */}
-      <div>
-        <p className="text-sm text-gray-300 leading-relaxed">
-          B-Block 4th Floor, COMMERCIAL BUILDING, UMA SREE DREAM WORLD, Unit -2,
-          Hosur Rd, Kudlu Gate, Industrial Layout, Garvebhavi Palya, Bengaluru,
-          Karnataka 560068
-        </p>
-      </div>
     </div>
   );
 }
