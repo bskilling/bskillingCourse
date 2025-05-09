058e2b97000a5fcd7e2a211276be0f127e0b71c5
rahul <dev02@sjbs.com>
2025-05-09 17:07:19 +0530

    rahul

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -36,7 +36,7 @@ export default function Login() {
 
   return (
     <div>
-      <h1 className="text-3xl text-center">Login App</h1>
+      <h1 className="text-3xl text-center">Login</h1>
     </div>
   );
 }
