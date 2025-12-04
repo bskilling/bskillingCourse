d66b724483f45ae8314886c9727282e78a4d4e21
sfjgit <ta7179@sfjbs.com>
2025-12-04 16:49:24 +0530

    rahul

--- next.config.js
+++ next.config.js
@@ -1,12 +1,35 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["https://www.bskilling.com/"],
-    loader: "akamai",
-    path: "/",
+    domains: ['www.bskilling.com'],
+    loader: 'akamai',
+    path: '/',
   },
   env: {
-    PUBLIC_URL: "/",
+    PUBLIC_URL: '/',
+  },
+
+  async redirects() {
+    return [
+      {
+        source: '/courses/course-details/prince2%C2%AE-7th-edition-foundation-certification',
+        destination: '/government-training-program/nasscom-future-skills/prince2-foundation',
+        permanent: true,
+      },
+      {
+        source:
+          '/courses/course-details/project-management-professional-(pmp)%C2%AE-certification-course',
+        destination:
+          '/government-training-program/nasscom-future-skills/project-management-professional-course',
+        permanent: true,
+      },
+      {
+        source: '/courses/course-details/itil%C2%AE-v4-foundation-certification',
+        destination:
+          '/government-training-program/nasscom-future-skills/itil-4-foundation-certification-training',
+        permanent: true,
+      },
+    ];
   },
 };
 
